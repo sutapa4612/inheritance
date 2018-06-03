37a48c8db37bfb37dd1e94ba9be18b7210e50c7f
sutapa4612 <39484717+sutapa4612@users.noreply.github.com>
2018-06-03 04:01:15 -0700

    Add files via upload

---
+++ bankmain.java
@@ -0,0 +1,28 @@
+class Bank{  
+int getRateOfInterest(){return 0;}  
+}  
+  
+class SBI extends Bank{  
+int getRateOfInterest(){return 8;}  
+}  
+  
+class ICICI extends Bank{  
+int getRateOfInterest(){return 7;}  
+}  
+class AXIS extends Bank{  
+int getRateOfInterest(){return 9;}  
+}  
+  
+class Bankmain{  
+public static void main(String args[]){  
+SBI s=new SBI();  
+ICICI i=new ICICI();  
+AXIS a=new AXIS();  
+s.getRateOfInterest();
+i.getRateOfInterest();
+a.getRateOfInterest();
+System.out.println("SBI Rate of Interest:"+s.getRateOfInterest());  
+System.out.println("ICICI Rate of Interest:" +i.getRateOfInterest());  
+System.out.println("AXIS Rate of Interest:"+a.getRateOfInterest());  
+}  
+}  
